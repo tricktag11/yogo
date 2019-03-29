e3a01791006258b511706ac02438e38d58b741ec
tricktag11 <48367527+tricktag11@users.noreply.github.com>
2019-03-29 13:21:31 -0400

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+/* recomended 40 sec */
+$set_delay=40;
+
+$config=[
+"imei" => "A100004233B965",
+"sign" => "ea654d8f778aec277ede75d365af57baa37e5f60",
+"uuid" => "c4ff62d834eaaf58",
+"token" => "29ba750f877c9b08d02a261ccf73c4c5"
+];
